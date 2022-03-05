fcfa163bd6edb3fa8357705941e6551c9a70ae45
Daniel Hilst <daniel@versatushpc.com.br>
2022-03-05 18:27:02 -0300

    Add anonymous function and blocks (sequence of expressions)

--- lampy3.py
+++ lampy3.py
@@ -13,14 +13,17 @@ grammar = r"""
     ?script : stmt | (stmt ";")+
 
     ?stmt : def_ | import_ | from_import | expr_1
-    ?expr_1 : let | ifelse | bin_expr | expr_2
+    ?expr_1 : let | ifelse | bin_expr | fun | expr_2
     ?expr_2 : appl
     ?expr_3 :  var | const | atom
 
-    def_            : "def"i CNAME ((CNAME+) | unit) "=" expr_1 "end"i?
+    block : expr_1 | (expr_1 ";")+
+
+    def_            : "def"i CNAME ((CNAME+) | unit) "=" block "end"i?
     from_import     : "from"i qname "import"i qname+
     import_          : "import"i qname
 
+    fun             : "fun"i (CNAME+ | unit) "=>" expr_1
     let             : "let"i CNAME "=" expr_1 "in" expr_1
     ifelse          : "if"i expr_1 "then"i expr_1 "else"i expr_1
     bin_expr : expr_1 (OP expr_1)+
@@ -110,13 +113,20 @@ class TFromImport(AST):
 class TImport(AST):
     module: str
 
+@dataclass
+class TBlock(AST):
+    exprs: List[AST]
 
 @dataclass
 class TDef(AST):
     name: str
     args: Union[List[TUnit], List[str]]
-    body: AST
+    body: TBlock
 
+@dataclass
+class TFun(AST):
+    args: Union[List[TUnit], List[str]]
+    body: AST
 
 @dataclass
 class TIfElse(AST):
@@ -181,9 +191,16 @@ class Transmformator(LarkTransformer):
     def qname(self, tree):
         return ".".join(tree)
 
+    def block(self, tree):
+        return TBlock(tree)
+
     def def_(self, tree):
         name, *args, body = tree
         return TDef(name, args, body)
+
+    def fun(self, tree):
+        *args, body = tree
+        return TFun(args, body)
 
     def ifelse(self, tree):
         return TIfElse(*tree)
@@ -216,8 +233,8 @@ def test_parse():
         "foo", "bar tar zar".split()
     )
 
-    assert parse("def id x = x end") == TDef("id", ["x"], TVar("x"))
-    assert parse("def const a b = a end") == TDef("const", ["a", "b"], TVar("a"))
+    assert parse("def id x = x end") == TDef("id", ["x"], TBlock(exprs=[TVar(name='x')]))
+    assert parse("def const a b = a end") == TDef("const", ["a", "b"], TBlock(exprs=[TVar(name='a')]))
 
     assert parse("if true then false else true") == TIfElse(
         TBool(True), TBool(False), TBool(True)
@@ -233,10 +250,11 @@ def test_parse():
     assert parse("foo ()") == TApply(fname=TVar(name="foo"), args=[TUnit()])
 
     assert parse("def foo () = 1") == TDef(
-        name=Token("CNAME", "foo"), args=[TUnit()], body=TInteger(value=1)
+        name=Token("CNAME", "foo"), args=[TUnit()], body=TBlock(exprs=[TInteger(value=1)])
     )
-
     assert parse("foo.bar ()") == TApply(fname=TVar(name="foo.bar"), args=[TUnit()])
+    assert parse("fun x => x") == TFun(args=[Token('CNAME', 'x')], body=TVar(name='x'))
+
 
 
 # Compiling stuff
@@ -274,6 +292,13 @@ def compile_py_expr(ast) -> str:
         return f"(lambda {ast.var} : {e2})({e1})"
     elif type(ast) is TIfElse:
         return f"{compile_py_expr(ast.then)} if {compile_py_expr(ast.cond)} else {compile_py_expr(ast.else_)}"
+    elif type(ast) is TFun:
+        body = compile_py_expr(ast.body)
+        if len(ast.args) == 1 and type(ast.args[0]) is TUnit:
+            return f"(lambda: {body})"
+        else:
+            return "(lambda {}: {})".format(", ".join(ast.args), body) # type: ignore
+
 
     raise RuntimeError(f"Compile error, {ast} not known")
 
@@ -299,11 +324,10 @@ def compile(ast, i=0) -> str:
             s.write(", ".join(ast.args))  # type: ignore
             s.write("):\n")
             s.write(indent(i))
-        body = compile(ast.body, i + 1)
-        body = [b for b in body.split("\n") if b]
-        body[-1] = re.sub(r"(\s+)(.*)", r"\1return \2\n", body[-1])
-        body = "\n".join(body)
-        s.write(body)
+
+        exprs = ["{}{}".format(indent(i+1), compile_py_expr(e)) for e in ast.body.exprs[:-1]]
+        exprs.append("{}return {}\n".format(indent(i+1), compile_py_expr(ast.body.exprs[-1])))
+        s.write("\n".join(exprs))
         return s.getvalue()
     elif type(ast) is TScript:
         exprs = [compile(e) for e in ast.exprs]
@@ -375,8 +399,7 @@ def id(x):
         pcompile("def foo x = let y = true in y")
         == """\
 def foo(x):
-    y = True
-    return y
+    return (lambda y : y)(True)
 """
     )
 
@@ -402,6 +425,8 @@ z
 """
     )
     assert pcompile("foo.bar ()") == "foo.bar()"
+
+    assert pcompile("fun x => x") == "(lambda x: x)"
 
 
 def main():
